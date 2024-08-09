d32a59a7f4fccd7e3f0349455c426220870ee780
rick <LinuxSuRen@users.noreply.github.com>
2024-08-09 02:04:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This project has integrated with some oauth2 providers, you can use it in your project.
+
+## Providers
+
+- [GitHub](https://github.com)
+- [Gitlab](https://gitlab.com)
+- Dex
+
+## Users
+- [api-testing](https://github.com/linuxsuren/api-testing)
---
+++ user.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2023 API Testing Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package oauth
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type OAuthProvider interface {
+	AllScopes() []string
+	MinimalScopes() []string
+	GetName() string
+	GetServer() string
+	SetServer(string)
+	GetTokenURL() string
+	GetAuthURL() string
+	GetUserInfoURL() string
+}
+
+var allOAuthProviders = make(map[string]OAuthProvider)
+
+func RegisterOAuthProvider(provier OAuthProvider) {
+	name := provier.GetName()
+	_, ok := allOAuthProviders[name]
+	if !ok {
+		allOAuthProviders[name] = provier
+	} else {
+		panic(fmt.Sprintf("duplicated oauth provider: %q", name))
+	}
+}
+
+func GetOAuthProvider(name string) OAuthProvider {
+	return allOAuthProviders[name]
+}
+
+type UserInfo struct {
+	Sub               string   `json:"sub"`
+	Name              string   `json:"name"`
+	PreferredUsername string   `json:"preferred_username"`
+	Email             string   `json:"email"`
+	Picture           string   `json:"picture"`
+	Groups            []string `json:"groups"`
+}
+
+func GetUserInfo(server OAuthProvider, token string, skipTlsVerify bool) (userInfo *UserInfo, err error) {
+	api := server.GetUserInfoURL()
+	if !strings.HasPrefix(api, "http://") && !strings.HasPrefix(api, "https://") {
+		api = fmt.Sprintf("%s%s", server.GetServer(), server.GetUserInfoURL())
+	}
+	req, err := http.NewRequest(http.MethodGet, api, nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := TlsAwareHTTPClient(skipTlsVerify)
+	var resp *http.Response
+	if resp, err = client.Do(req); err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Printf("getting userinfo from %s\n", server.GetName())
+	if resp.StatusCode == http.StatusOK {
+		var data []byte
+		if data, err = io.ReadAll(resp.Body); err != nil {
+			return
+		}
+
+		userInfo = &UserInfo{}
+		err = json.Unmarshal(data, userInfo)
+	}
+	return
+}
+
+func TlsAwareHTTPClient(insecure bool) *http.Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+	}
+	return &http.Client{Transport: tr}
+}
